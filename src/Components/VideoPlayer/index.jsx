8ca2408fbc94e41efdc9129c6efce4e9c284b5ff
Kavie123 <kaveeshasandaruwan1111@gmail.com>
2024-04-03 00:37:08 +0530

    video player

---
+++ src/Components/VideoPlayer/index.jsx
@@ -0,0 +1,24 @@
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+
+const VideoPlayer = ({ filename }) => {
+    return (
+        <MediaPlayer src={`/Videos/${filename}`} aspectRatio="16/9">
+            <MediaProvider />
+            <DefaultVideoLayout
+                icons={defaultLayoutIcons}
+                style={{
+                    '--video-brand': '#f5f5f5',
+                }}
+                slots={{
+                    googleCastButton: null,
+                }}
+            />
+        </MediaPlayer>
+    );
+}
+
+export default VideoPlayer;
